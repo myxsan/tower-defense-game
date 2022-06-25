937c294a2232c141c74f538deeac21ec8da2d247
myxsan <yksl3461@gmail.com>
2022-06-26 02:30:50 +0300

    Set up turret shooting system and organised folders

--- Tower-Defense-Game-Project/Assets/Scripts/Turret.cs
+++ Tower-Defense-Game-Project/Assets/Scripts/Turret.cs
@@ -5,9 +5,21 @@ using UnityEngine;
 public class Turret : MonoBehaviour
 {
     private Transform target;
-    public Transform partToRotate;
+
+    [Header("Attributes")]
+
     public float range = 15f;
+    public float fireRate = 1f;
+    private float fireCountdown = 0f;
+
+    [Header("Unity Setup Field")]
+    public string enemyTag = "Enemy";
+
+    public Transform partToRotate;
     public float turnSpeed = 10f;
+
+    public GameObject bulletPrefab;
+    public Transform firePoint;
 
     void Start()
     {
@@ -16,7 +28,7 @@ public class Turret : MonoBehaviour
 
     void UpdateTarget()
     {
-        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
 
         float shortestDistance = Mathf.Infinity;
         GameObject nearestEnemy = null;
@@ -45,13 +57,37 @@ public class Turret : MonoBehaviour
 
     void Update()
     {
-        if(target == null) return;
-        
+        if (target == null) return;
+
+        GetTargetDirection();
+
+        if (fireCountdown <= 0f)
+        {
+            Shoot();
+            fireCountdown = 1f / fireRate;
+        }
+
+        fireCountdown -= Time.deltaTime;
+    }
+
+    private void GetTargetDirection()
+    {
         Vector3 direction = target.position - transform.position;
         Quaternion lookRotation = Quaternion.LookRotation(direction);
         Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
 
         partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+    }
+
+    private void Shoot()
+    {
+        GameObject bulletGO = (GameObject)Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Bullet bullet = bulletGO.GetComponent<Bullet>();
+
+        if(bullet != null)
+        {
+            bullet.Seek(target);
+        }
     }
 
     private void OnDrawGizmosSelected() 
