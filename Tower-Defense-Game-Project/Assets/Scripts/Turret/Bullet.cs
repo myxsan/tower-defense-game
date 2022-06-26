240294e22e2d2d3ef7dfdd2ba1891fe7136026a3
myxsan <yksl3461@gmail.com>
2022-06-27 02:53:51 +0300

    Set up missles

--- Tower-Defense-Game-Project/Assets/Scripts/Turret/Bullet.cs
+++ Tower-Defense-Game-Project/Assets/Scripts/Turret/Bullet.cs
@@ -8,6 +8,8 @@ public class Bullet : MonoBehaviour
     private Transform target;
 
     public float speed = 70f;
+    public float explosionRadius = 0f;
+
     public GameObject impactVFX;
 
     public void Seek(Transform _target)
@@ -32,14 +34,46 @@ public class Bullet : MonoBehaviour
         }
 
         transform.Translate(direction.normalized * distanceThisFrame, Space.World);
+        transform.LookAt(target);
 
     }
 
     private void HitTarget()
     {
         GameObject effectIns = (GameObject)Instantiate(impactVFX, transform.position, transform.rotation);
-        Destroy(effectIns, 2f);
+        Destroy(effectIns, 5f);
+
+        if(explosionRadius > 0f)
+        {
+            Explode();
+        } else
+        {
+            Damage(target);
+        }
 
         Destroy(gameObject);
+    }
+
+    private void Explode()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
+        foreach(Collider collider in colliders)
+        {
+            if(collider.tag == "Enemy")
+            {
+                Damage(collider.transform);
+            }
+        }
+
+    }
+
+    private void Damage(Transform enemy)
+    {
+        Destroy(enemy.gameObject);
+    }
+
+    private void OnDrawGizmosSelected() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
     }
 }
