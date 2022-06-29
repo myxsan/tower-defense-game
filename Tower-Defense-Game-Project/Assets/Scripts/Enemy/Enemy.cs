107541e47059a80165a7c9adb868671c510e5fc9
myxsan <yksl3461@gmail.com>
2022-06-29 14:29:29 +0300

    Set up enemy health bar

--- Tower-Defense-Game-Project/Assets/Scripts/Enemy/Enemy.cs
+++ Tower-Defense-Game-Project/Assets/Scripts/Enemy/Enemy.cs
@@ -1,23 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
     public float startSpeed = 10f;
     [HideInInspector] public float speed;
-    public float health = 100;
+    public float startHealth = 100;
+    public float health;
     public int worth = 50;
     public GameObject deathVFX;
 
+    [Header("Unity Stuff")]
+    public Image healthBar;
+
     private void Start()
     {
+        health = startHealth;
         speed = startSpeed;
     }
 
     public void TakeDamage(float amount)
     {
         health -= amount;
+
+        healthBar.fillAmount = health / startHealth;
 
         if(health <= 0)
         {
