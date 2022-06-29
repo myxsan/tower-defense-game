38f237e334bd82bc426da44eccb3e65fc2a6cf88
myxsan <yksl3461@gmail.com>
2022-06-29 03:58:11 +0300

    Created pause menu and button animation

--- Tower-Defense-Game-Project/Assets/Scripts/Managing/CameraController.cs
+++ Tower-Defense-Game-Project/Assets/Scripts/Managing/CameraController.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    private bool doMovement = true;
 
     public float panSpeed = 30f;
     public float panBorderThickness = 10f;
@@ -21,10 +20,6 @@ public class CameraController : MonoBehaviour
             this.enabled = false;
             return;
         }
-
-
-        if (Input.GetKeyDown(KeyCode.Escape)) doMovement = !doMovement;
-        if (!doMovement) return;
 
         SetMoveInput();
         ZoomInAndOut();
